dc242a328697abfea5d6205f1ea68174e896d2d9
qubitAI <osmanmorales@ymail.com>
2018-12-27 00:57:32 -0500

    Update network.cpp

--- network.cpp
+++ network.cpp
@@ -4,7 +4,7 @@ Network::Network(const tuple<int, int>&insize, const tuple<int, int>&outsize, co
 const double& lamb=0.5, const double&alph)
 :
 {
-
+layerInputs(make_shared<vector<Mat<double>>>(vector<Mat<double>>({Mat<double>(get<0>())}))) 
 
 
 
