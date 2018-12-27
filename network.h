3533acc4fc5877b01bf8e119151b93db216dbebb
qubitAI <osmanmorales@ymail.com>
2018-12-27 00:35:13 -0500

    Create network.h

---
+++ network.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <armadillo>
+#include <vector>
+#include <utility>
+#include <tuple>
+#include <memory>
+#include <sstream>
+#include <list>
+#include <random>
+#include <cmath>
+
