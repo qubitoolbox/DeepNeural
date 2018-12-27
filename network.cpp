572725acdfb6b83865291378294db5cad92438d6
qubitAI <osmanmorales@ymail.com>
2018-12-27 00:53:20 -0500

    Create network.cpp

---
+++ network.cpp
@@ -0,0 +1,11 @@
+#include "network.h"
+
+Network::Network(const tuple<int, int>&insize, const tuple<int, int>&outsize, const string& act="relu",
+const double& lamb=0.5, const double&alph)
+:
+{
+
+
+
+
+}
